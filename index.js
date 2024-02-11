76d4c62477ff001b87946fa12b39c76857baf886
Songpob Hamthanan <jsongpob@gmail.com>
2024-02-11 16:29:09 +0700

    Completed feature #1

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-import express from "express";
-import mysql from "mysql";
+import express from 'express';
 const app = express();
 
 const PORT = 8080;
-
-const con = mysql.createConnection({
-    host: "popdb.cpik6822ykjd.us-east-1.rds.amazonaws.com",
-    user: "admin",
-    password: "fryiXohCyL9A07r8QfAg"
-});
 
 app.listen(PORT, () => {
     console.log(`Running on PORT ${PORT}`);
@@ -18,7 +11,9 @@ app.post('/users', (req, res) => {
     if (req.query.username && req.query.email && req.query.age) {
         console.log('Request received');
         con.connect(function(err) {
-            con.query(`INSERT INTO main.users (username, email, age) VALUES ('+${req.query.username}','${req.query.email}','${req.query.age}')`,function(err, result, fields) {
+            con.query(`INSERT INTO main.users (username, email, age) VALUES ('+
+            ${req.query.username}', '${req.query.email}', '${req.query.age}')`, 
+            function(err, result, fields) {
                 if (err) res.send(err);
                 if (result) res.send({username: req.query.username, email: req.query.email, age: req.query.age});
                 if (fields) console.log(fields);
@@ -37,4 +32,3 @@ app.get('/users', (req, res) => {
         });
     });
 });
-
